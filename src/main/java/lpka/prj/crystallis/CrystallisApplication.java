04384e5bb0b8df77e6954d270bac1f60f7066039
StromenkoNA <n.stromenko@id-mt.ru>
2022-11-29 00:54:10 +0800

    first commit

---
+++ src/main/java/lpka/prj/crystallis/CrystallisApplication.java
@@ -0,0 +1,13 @@
+package lpka.prj.crystallis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystallisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrystallisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lpka/prj/crystallis/CrystallisApplicationTests.java
@@ -0,0 +1,13 @@
+package lpka.prj.crystallis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystallisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
